5dd18f64f2abec9e58b6ee802248742c8d4eca81
Taggartjoey9 <taggartjoey9@gmail.com>
2024-11-22 08:10:10 -0500

    seting up the UI for inputs

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,16 @@
+export default function UserInput() {
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input />
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input />
+                </p>
+            </div>
+        </section>
+    )
+}
